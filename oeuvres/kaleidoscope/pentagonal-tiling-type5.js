ddb88d9488c5e4689e922fd42d29d73434eeaf35
dnauu-bmsotc <87213021+dnauu-bmsotc@users.noreply.github.com>
2023-02-05 13:21:52 +0300

    kaleidoscope: update
    function createPentagonalTilingType5 rewritten as a class

--- oeuvres/kaleidoscope/pentagonal-tiling-type5.js
+++ oeuvres/kaleidoscope/pentagonal-tiling-type5.js
@@ -1,61 +1,112 @@
 "use strict"
 
-function createPentagonalTilingType5(svgcanvas, fill, start, size, count, parameterAngle, parameterSegment) {
-    function createPrimitiveCell(center) {
-        const cell = document.createElementNS("http://www.w3.org/2000/svg", "g");
-        function createSingleTile(center, angle) {
-            const tile = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-            const points = [svgcanvas.createSVGPoint()];
-            points[0].x = center.x;
-            points[0].y = center.y;
-            function addPointOff(distance, angle) {
-                const p = svgcanvas.createSVGPoint();
-                p.x = points[points.length - 1].x;
-                p.y = points[points.length - 1].y;
-                p.x += distance * Math.cos(angle / 180 * Math.PI);
-                p.y += distance * Math.sin(angle / 180 * Math.PI);
-                points.push(p);
-            }
-            addPointOff(size, angle);
-            addPointOff(parameterSegment, angle - (180 - parameterAngle));
-            const u = (parameterAngle - 60) * Math.PI / 180;
-            const x = Math.sqrt(size**2 + parameterSegment**2 - 2*size*parameterSegment*Math.cos(u));
-            const a = Math.asin(size / x * Math.sin(u)) * 180 / Math.PI + 30;
-            addPointOff(x / Math.sqrt(3), angle - (180 - parameterAngle) - (180 - a));
-            addPointOff(x / Math.sqrt(3), angle - (180 - parameterAngle) - (180 - a) - 60);
-            for (let point of points) {
-                tile.points.appendItem(point);
-            }
-            tile.style.fill = fill;
-            return tile;
-        }
-        const base_segment2_opposite_side = Math.sqrt(size**2 + (parameterSegment/2)**2 - 2*size*(parameterSegment/2)*Math.cos(parameterAngle*Math.PI/180));
-        const additionalRotation = Math.asin((parameterSegment/2)/base_segment2_opposite_side*Math.sin(parameterAngle*Math.PI/180)) * 180 / Math.PI;
+class PentagonalTilingType5 {
+    constructor({size1, size2, angle, nLayers}) {
+        this.parameters = {
+            s1: size1,
+            s2: size2,
+            a: angle,
+            n: nLayers
+        };
+        this.cells = [];
+        this.createTiling();
+    }
+    cos(degrees) {
+        return Math.cos(degrees / 180 * Math.PI);
+    }
+    sin(degrees) {
+        return Math.sin(degrees / 180 * Math.PI);
+    }
+    asin(sin) {
+        return Math.asin(sin) / Math.PI * 180;
+    }
+    pushPointTowards(arr, distance, angle) {
+        arr.push({
+            x: arr[arr.length - 1].x + distance * this.cos(angle),
+            y: arr[arr.length - 1].y + distance * this.sin(angle)
+        });
+    }
+    createSingleTile() {
+        const points = [{x: 0, y: 0}];
+        const p = this.parameters;
+        const v = Math.sqrt(p.s1**2 + p.s2**2 - 2*p.s1*p.s2*this.cos(p.a - 60));
+        const u = this.asin(p.s1 / v * this.sin(p.a - 60)) + 30;
+        this.pushPointTowards(points, p.s1, 0);
+        this.pushPointTowards(points, p.s2, p.a - 180);
+        this.pushPointTowards(points, v / Math.sqrt(3), p.a + u - 360);
+        this.pushPointTowards(points, v / Math.sqrt(3), p.a + u - 420);
+        return points;
+    }
+    createPrimitiveCell(x, y) {
+        const cell = {
+            x: x,
+            y: y,
+            tiles: []
+        };
+        const p = this.parameters;
+        const v = Math.sqrt(p.s1**2 + (p.s2/2)**2 - 2*p.s1*(p.s2/2)*this.cos(p.a));
+        const r = this.asin((p.s2/2) / v * this.sin(p.a));
         for (let rotation = 0; rotation < 360; rotation += 60) {
-            const tile = createSingleTile({x: 0, y: 0}, 0);
-            tile.setAttribute("transform", `translate(${center.x}, ${center.y}) rotate(${rotation + additionalRotation})`);
-            tile.style.stroke = "black";
-            tile.style.strokeWidth = "0.1";
-            cell.appendChild(tile);
+            cell.tiles.push(this.createSingleTile());
+            cell.tiles[cell.tiles.length - 1].transform = {
+                rotate: rotation + r
+            };
         }
-        svgcanvas.appendChild(cell);
+        return cell;
     }
-    const a = parameterAngle*Math.PI/180 - Math.PI/3;
-    const s1 = size**2 * Math.sqrt(3) / 4;
-    const s2 = 1/2*size*parameterSegment*Math.sin(a);
-    const s3 = Math.sqrt(3)/12*(size**2+parameterSegment**2-2*size*parameterSegment*Math.cos(a));
-    const s = 6*(s1+s2+s3);
-    const step = Math.pow(108, 1/4)/3*Math.sqrt(s);
-
-    createPrimitiveCell(start);
-    for (let layer = 1; layer < count; layer++) {
-        for (let angle = 0; angle < 2*Math.PI; angle += 2*Math.PI/6) {
-            for (let i = 0; i < layer; i++) {
-            createPrimitiveCell({
-                x: start.x + layer*step*Math.cos(angle) + i*step*Math.cos(angle + 2*Math.PI/3),
-                y: start.y + layer*step*Math.sin(angle) + i*step*Math.sin(angle + 2*Math.PI/3)
-            });
+    createTiling() {
+        const p = this.parameters;
+        const s = 6 * (p.s1**2*Math.sqrt(3)/4 + 1/2*p.s1*p.s2*this.sin(p.a-60) + Math.sqrt(3)/12*(p.s1**2+p.s2**2-2*p.s1*p.s2*this.cos(p.a-60)));
+        const step = Math.pow(108, 1/4)/3*Math.sqrt(s);
+        this.cells.push(this.createPrimitiveCell(0, 0));
+        for (let layer = 1; layer < p.n; layer++) {
+            for (let angle = 0; angle < 360; angle += 60) {
+                for (let i = 0; i < layer; i++) {
+                    this.cells.push(this.createPrimitiveCell(
+                        layer*step*this.cos(angle) + i*step*this.cos(angle + 120),
+                        layer*step*this.sin(angle) + i*step*this.sin(angle + 120)
+                    ));
+                }
             }
         }
     }
+    deployAtSVG(svgcanvas, x, y, tileStyle) {
+        for (let cell of this.cells) {
+            cell.el = document.createElementNS("http://www.w3.org/2000/svg", "g");
+            for (let tile of cell.tiles) {
+                tile.el = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+                for (let point of tile) {
+                    const p = svgcanvas.createSVGPoint();
+                    p.x = point.x;
+                    p.y = point.y;
+                    tile.el.points.appendItem(p);
+                    for (let k in tileStyle) {
+                        tile.el.style[k]=tileStyle[k];
+                    }
+                }
+                tile.el.setAttribute("transform", `rotate(${tile.transform.rotate})`);
+                cell.el.appendChild(tile.el);
+                tile.animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+                tile.el.appendChild(tile.animate);
+            }
+            cell.el.setAttribute("transform", `translate(${x + cell.x}, ${y + cell.y})`);
+            svgcanvas.appendChild(cell.el);
+            cell.animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+            cell.el.appendChild(cell.animate);
+        }
+    }
+    // animate(size1, size2, angle, callback) {
+    //     this.cells = [];
+    //     this.parameters.s1 = size1;
+    //     this.parameters.s2 = size2;
+    //     this.parameters.a = angle;
+    //     this.createTiling();
+    //     for (let cell of this.cells) {
+    //         const animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+    //         animate.attributeName = "transform";
+    //         animate.from = cell.transform;
+    //         animate.to = `translate(${0}, ${0})`;
+    //         cell.animate.replaceWith(animate);
+    //     }
+    // }
 }
