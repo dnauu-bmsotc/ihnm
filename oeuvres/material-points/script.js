a8b5509ec5d7132bdd786363fb80937e8e7007a5
dnauu-bmsotc <87213021+dnauu-bmsotc@users.noreply.github.com>
2023-05-12 23:57:51 +0300

    material-point update
    1. рассчёты больше не производятся для двух точек дважды;
    2. частицы теперь не должны улетать за экран;
    3. пресеты теперь не влиюят на опции;
    4. добавлены варианты расчёта гравитации: стандартный и урезанный;
    5. обновлены стили;
    6. добавлен режим слипания;
    7. добавлен режим статичных массивных точек.

--- oeuvres/material-points/script.js
+++ oeuvres/material-points/script.js
@@ -6,20 +6,30 @@ class Point {
 }
 
 class MaterialDot {
-    constructor(x, y, mass) {
+    constructor(x, y, mass, imaginary=false) {
         this.position = new Point(x, y);
         this.velocity = new Point(0, 0);
         this.forces = new Point(0, 0);
-        this.mass = mass;
 
         this.element = document.createElementNS(display.ns, "circle");
         this.element.setAttributeNS(null, "fill", "white");
         this.element.setAttributeNS(null, "cx", this.position.x);
         this.element.setAttributeNS(null, "cy", this.position.y);
+        this.element.setAttributeNS(null, "stroke-width", "0");
+
+        this.mass = mass;
+
+        if (!imaginary) {
+            document.getElementById("dots").appendChild(this.element);
+        }
+    }
+    get mass() {
+        return this._mass;
+    }
+    set mass(value) {
+        this._mass = value;
         this.element.setAttributeNS(null, "r", 0.2 + Math.sqrt(this.mass / display.maxMass) / 3);
         this.element.setAttributeNS(null, "opacity", (display.maxMass/3 + this.mass) / (display.maxMass/3 + display.maxMass));
-        this.element.setAttributeNS(null, "stroke-width", "0");
-        document.getElementById("dots").appendChild(this.element);
     }
 }
 
@@ -36,6 +46,8 @@ const display = {
     gravity: 10 ** (-6),
     minMass: 1,
     maxMass: 500,
+    options: {},
+    lastResetFunction: null,
 }
 
 function randomInt(min, max) {
@@ -58,8 +70,9 @@ function hslToHex(h, s, l) {
     return `#${f(0)}${f(8)}${f(4)}`;
 }
 
-function randomMaterialDot(maxradius, initialVelocity) {
-    const radius = randomFloat(0, maxradius);
+function randomMaterialDot() {
+    const maxRadius = 10 + 10 * Math.pow(display.options.numberOfPoints, 1/4);
+    const radius = randomFloat(0, maxRadius);
     const angle = randomFloat(0, 2 * Math.PI);
 
     const dot = new MaterialDot(
@@ -68,23 +81,9 @@ function randomMaterialDot(maxradius, initialVelocity) {
         display.minMass + Math.pow(Math.random(), 8) * (display.maxMass - display.minMass),
     );
 
-    switch (initialVelocity) {
-        case "noise":
-            dot.velocity.x = randomFloat(-0.012, +0.012);
-            dot.velocity.y = randomFloat(-0.012, +0.012);
-            break;
-
-        case "funnel":
-            const additionalAngle = Math.PI / 4 * randomFloat(-1, +1);
-            const velocityAngle = angle + display.funnelBaseAngle + additionalAngle;
-            const velocity = randomFloat(0, +0.03);
-            dot.velocity.x = velocity * Math.cos(velocityAngle);
-            dot.velocity.y = velocity * Math.sin(velocityAngle);
-            break;
-    
-        default:
-            break;
-    }
+    display.options.velocityCallback(
+        dot, { angle: angle, radius: radius }
+    );
 
     return dot;
 }
@@ -114,13 +113,7 @@ function animateFrame() {
         dot.forces.y = 0;
     }
 
-    for (let dot1 of display.dots) {
-        for (let dot2 of display.dots) {
-            if (dot1 !== dot2) {
-                lawOfGravity(dot1, dot2);
-            }
-        }
-    }
+    display.options.calculationCallback();
 
     for (let dot of display.dots) {
         dot.velocity.x += dot.forces.x / dot.mass * dt;
@@ -132,8 +125,8 @@ function animateFrame() {
         dot.element.setAttributeNS(null, "cx", dot.position.x);
         dot.element.setAttributeNS(null, "cy", dot.position.y);
 
-        display.colorFunction(dot);
-        display.borderFunction(dot);
+        display.options.colorCallback(dot);
+        display.options.borderCallback(dot);
     }
 
     if (display.active) {
@@ -141,60 +134,189 @@ function animateFrame() {
     }
 }
 
-function restart(createPoints=true) {
-    document.getElementById("dots").innerHTML = "";
-    display.dots = [];
+function setVelocityCallback(name) {
+    switch (name) {
+        case "funnel":
+            const baseAngle = Math.sign(randomFloat(-1, +1)) * Math.PI / 2 + Math.PI / 4 * randomFloat(-1, +1);
+            display.options.velocityCallback = function(dot, polar) {
+                const additionalAngle = Math.PI * randomFloat(-1/10, +1/10);
+                const velocityAngle = polar.angle + baseAngle + additionalAngle;
+                const velocity = randomFloat(0.005, +0.02) / Math.pow(dot.mass, 1/6) / (0.5 + (polar.radius / 50)**2/2);
+                dot.velocity.x = velocity * Math.cos(velocityAngle);
+                dot.velocity.y = velocity * Math.sin(velocityAngle);
+            };
+            break;
+        
+        case "noise":
+            display.options.velocityCallback = function(dot) {
+                dot.velocity.x = randomFloat(-0.012, +0.012);
+                dot.velocity.y = randomFloat(-0.012, +0.012);
+            }
+            break;
 
-    const initialVelocity = document.getElementById("velocity-select").value;
-    if (initialVelocity === "funnel") {
-        display.funnelBaseAngle = Math.sign(randomFloat(-1, +1)) * Math.PI / 2
-            + Math.PI / 4 * randomFloat(-1, +1);
+        default:
+            display.options.velocityCallback = () => {};
+            break;
     }
+}
 
-    const n = createPoints ? document.getElementById("number-select").value : 0;
-    const radius = 10 + 10 * Math.pow(n, 1/4);
-
-    switch (document.getElementById("border-select").value) {
+function setBorderCallback(name) {
+    switch (name) {
         case "bounce":
-            display.borderFunction = dot => {
-                if (dot.position.x < display.rect.left) dot.velocity.x *= -1;
-                if (dot.position.x > display.rect.left + display.rect.right) dot.velocity.x *= -1;
-                if (dot.position.y < display.rect.top) dot.velocity.y *= -1;
-                if (dot.position.y > display.rect.top + display.rect.bottom) dot.velocity.y *= -1;
+            display.options.borderCallback = dot => {
+                if (dot.position.x < display.rect.left) {
+                    dot.velocity.x = Math.abs(dot.velocity.x);
+                }
+                else if (dot.position.x > display.rect.left + display.rect.right) {
+                    dot.velocity.x = -Math.abs(dot.velocity.x);
+                }
+                else if (dot.position.y < display.rect.top) {
+                    dot.velocity.y = Math.abs(dot.velocity.y);
+                }
+                else if (dot.position.y > display.rect.top + display.rect.bottom) {
+                    dot.velocity.y = -Math.abs(dot.velocity.y);
+                }
             };
             break;
 
         case "wall":
-            display.borderFunction = dot => {
-                if (dot.position.x < display.rect.left) dot.velocity.x = 0;
-                if (dot.position.x > display.rect.left + display.rect.right) dot.velocity.x = 0;
-                if (dot.position.y < display.rect.top) dot.velocity.y = 0;
-                if (dot.position.y > display.rect.top + display.rect.bottom) dot.velocity.y = 0;
+            display.options.borderCallback = dot => {
+                if (dot.position.x < display.rect.left) {
+                    dot.position.x = display.rect.left;
+                    dot.velocity.x = 0;
+                }
+                else if (dot.position.x > display.rect.left + display.rect.right) {
+                    dot.position.x = display.rect.left + display.rect.right;
+                    dot.velocity.x = 0;
+                }
+                else if (dot.position.y < display.rect.top) {
+                    dot.position.y = display.rect.top;
+                    dot.velocity.y = 0;
+                }
+                else if (dot.position.y > display.rect.top + display.rect.bottom) {
+                    dot.position.y = display.rect.top + display.rect.bottom;
+                    dot.velocity.y = 0;
+                }
             }
             break;
         
         default:
-            display.borderFunction = () => {};
+            display.options.borderCallback = () => {};
             break;
     }
+}
 
-    switch (document.getElementById("color-select").value) {
+function setColorCallback(name) {
+    switch (name) {
         case "wheel":
-            display.colorFunction = dot => {
+            display.options.colorCallback = dot => {
                 const velocityAngle = Math.atan2(dot.velocity.y, dot.velocity.x) * 180 / Math.PI;
                 dot.element.setAttributeNS(null, "fill", hslToHex(velocityAngle, 100, 50));
             };
             break;
         
         default:
-            display.colorFunction = () => {};
+            display.options.colorCallback = () => {};
             break;
     }
+}
 
-    for (let i = 0; i < n; i++) {
-        display.dots.push(randomMaterialDot(radius, initialVelocity));
+function setCalculationCallback(name) {
+    switch (name) {
+        case "massive":
+            display.options.calculationCallback = function() {
+                for (let i = 0; i < display.dots.length; i++) {
+                    if (display.dots[i].mass > (display.minMass + display.maxMass) / 2) {
+                        for (let j = i + 1; j < display.dots.length; j++) {
+                            lawOfGravity(display.dots[i], display.dots[j]);
+                            display.options.collisionCallback(display.dots[i], display.dots[j]);
+                        }
+                    }
+                }
+            }
+            break;
+
+        case "staticmassive":
+            display.options.calculationCallback = function() {
+                for (let i = 0; i < display.dots.length; i++) {
+                    for (let j = i + 1; j < display.dots.length; j++) {
+                        lawOfGravity(display.dots[i], display.dots[j]);
+                        display.options.collisionCallback(display.dots[i], display.dots[j]);
+                    }
+                }
+                for (let dot of display.dots) {
+                    if (dot.mass > (display.minMass + display.maxMass) * 7 / 10) {
+                        dot.forces.x = dot.forces.y = 0;
+                        dot.velocity.x = dot.velocity.y = 0;
+                    }
+                }
+            }
+            break;
+    
+        default:
+            display.options.calculationCallback = function() {
+                for (let i = 0; i < display.dots.length; i++) {
+                    for (let j = i + 1; j < display.dots.length; j++) {
+                        lawOfGravity(display.dots[i], display.dots[j]);
+                        display.options.collisionCallback(display.dots[i], display.dots[j]);
+                    }
+                }
+            };
+            break;
+    }
+}
+
+function setCollisionCallback(name) {
+    switch (name) {
+        case "stick":
+            display.options.collisionCallback = function(d1, d2) {
+                const dx = d2.position.x - d1.position.x;
+                const dy = d2.position.y - d1.position.y;
+                if (dx**2 + dy**2 < 0.2) {
+                    d1.velocity.x = (d1.mass * d1.velocity.x + d2.mass * d2.velocity.x) / (d1.mass + d2.mass);
+                    d1.velocity.y = (d1.mass * d1.velocity.y + d2.mass * d2.velocity.y) / (d1.mass + d2.mass);
+
+                    d1.position.x = (d2.position.x + d1.position.x) / 2;
+                    d1.position.y = (d2.position.y + d1.position.y) / 2;
+                    d1.mass += d2.mass;
+
+                    d2.element.remove();
+                    display.dots.splice(display.dots.indexOf(d2), 1);
+                }
+            }
+            break;
+
+        default:
+            display.options.collisionCallback = () => {};
+            break;
+    }
+}
+
+function restartToUserOptions() {
+    clear();
+
+    display.options.numberOfPoints = +document.getElementById("number-select").value;
+
+    setVelocityCallback(document.getElementById("velocity-select").value);
+    setBorderCallback(document.getElementById("border-select").value);
+    setColorCallback(document.getElementById("color-select").value);
+    setCalculationCallback(document.getElementById("calculation-select").value);
+    setCollisionCallback(document.getElementById("collision-select").value);
+
+    for (let i = 0; i < display.options.numberOfPoints; i++) {
+        display.dots.push(randomMaterialDot());
     }
 
+    display.lastResetFunction = restartToUserOptions;
+    startAnimation();
+}
+
+function clear() {
+    document.getElementById("dots").innerHTML = "";
+    display.dots = [];
+}
+
+function startAnimation() {
     if (!display.active) {
         requestAnimationFrame(animateFrame);
     }
@@ -203,29 +325,27 @@ function restart(createPoints=true) {
     display.previousTimestamp = performance.now();
 }
 
-window.addEventListener("load", (event) => {
-    preset0();
-});
-
-function preset0() {
+function reset() {
     document.getElementById("border-select").value = "wall";
     document.getElementById("color-select").value = "white";
     document.getElementById("number-select").value = "100";
     document.getElementById("velocity-select").value = "static";
+    document.getElementById("calculation-select").value = "all";
+    document.getElementById("collision-select").value = "skip";
 
-    restart(true);
+    restartToUserOptions();
 }
 
 function preset1() {
-    document.getElementById("border-select").value = "none";
-    document.getElementById("color-select").value = "wheel";
-    document.getElementById("number-select").value = "preset";
-    document.getElementById("velocity-select").value = "preset";
+    clear();
 
-    restart(false);
-
-    const n = 12;
-    const r = 25;
+    setBorderCallback("bounce");
+    setColorCallback("wheel");
+    setCalculationCallback("all");
+    setCollisionCallback("stick");
+    
+    const n = 16;
+    const r = 20;
     const v = 0.01;
     for (let i = 0; i < n; i++) {
         const polarAngle = i * (360 / n) * Math.PI / 180;
@@ -242,25 +362,35 @@ function preset1() {
 
         display.dots.push(dot);        
     }
+
+    display.lastResetFunction = preset1;
+    startAnimation();
 }
 
 function preset2() {
-    document.getElementById("border-select").value = "none";
-    document.getElementById("color-select").value = "white";
-    document.getElementById("number-select").value = "preset";
-    document.getElementById("velocity-select").value = "preset";
+    clear();
 
-    restart(false);
+    setBorderCallback("none");
+    setColorCallback("white");
+    setCalculationCallback("all");
+    setCollisionCallback("skip");
 
     const sun = new MaterialDot(0, 0, display.maxMass * 100);
-    sun.velocity.y = +0.005;
+    sun.velocity.y = +0.003;
     display.dots.push(sun);
 
     const earth = new MaterialDot(40, 0, display.maxMass * 8.1);
-    earth.velocity.y = -0.05;
+    earth.velocity.y = -0.03;
     display.dots.push(earth);
 
     const moon = new MaterialDot(45, 0, display.maxMass / 2);
-    moon.velocity.y = -0.08;
+    moon.velocity.y = -0.05;
     display.dots.push(moon);
+
+    display.lastResetFunction = preset2;
+    startAnimation();
 }
+
+window.addEventListener("load", (event) => {
+    reset();
+});
